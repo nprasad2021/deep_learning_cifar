8f38e2550fa40d376a23866329b8b467b6b8e745
Neeraj Prasad <nprasad@mit.edu>
2018-01-22 12:03:43 +0300

    dl file

---
+++ fhw.py
@@ -0,0 +1,241 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 22 11:16:37 2018
+
+@author: neeraj
+"""
+
+import numpy as np
+import pandas as pd
+
+import keras
+from keras.datasets import cifar10
+from keras.models import *
+from keras.layers import *
+from keras.optimizers import *
+from keras.preprocessing.image import ImageDataGenerator
+
+from keras import layers
+from keras.layers import Input, Add, Dense, Activation, ZeroPadding2D, BatchNormalization, Flatten, Conv2D, AveragePooling2D, MaxPooling2D, GlobalMaxPooling2D
+from keras.models import Model, load_model
+from keras.preprocessing import image
+from keras.utils import layer_utils
+from keras.utils.data_utils import get_file
+from keras.applications.imagenet_utils import preprocess_input
+
+
+#----------------------------------------------------------
+# Configure Input Data
+#----------------------------------------------------------
+(X_train, y_train), (X_test, _) = cifar10.load_data()
+
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+
+X_train_unmod = X_train
+X_test_unmod = X_test
+
+X_train /= 255
+X_test /= 255
+
+print('x_train shape:', X_train.shape)
+print(X_train.shape[0], 'train samples')
+print(X_test.shape[0], 'test samples')
+
+#X_train = 2*(X_train - X_train.min()) / (X_train.max() - X_train.min())
+
+
+y_train = keras.utils.to_categorical(y_train)
+
+# Image Data Generator
+'''
+datagen_train = ImageDataGenerator(
+    featurewise_center=True,
+    featurewise_std_normalization=True)
+
+datagen_train.fit(X_train)
+'''
+
+#----------------------------------------------------------
+#-----------------------------Model------------------------
+#----------------------------------------------------------
+
+def identity_block(X, f, filters, stage, block):
+    """
+    Implementation of the identity block
+    
+    Arguments:
+    X -- input tensor of shape (m, n_H_prev, n_W_prev, n_C_prev)
+    f -- integer, specifying the shape of the middle CONV's window for the main path
+    filters -- python list of integers, defining the number of filters in the CONV layers of the main path
+    stage -- integer, used to name the layers, depending on their position in the network
+    block -- string/character, used to name the layers, depending on their position in the network
+    
+    Returns:
+    X -- output of the identity block, tensor of shape (n_H, n_W, n_C)
+    """
+    
+    # defining name basis
+    conv_name_base = 'res' + str(stage) + block + '_branch'
+    bn_name_base = 'bn' + str(stage) + block + '_branch'
+    
+    # Retrieve Filters
+    F1, F2, F3 = filters
+    X_shortcut = X
+    
+    
+    X = Conv2D(filters = F1, kernel_size = (1, 1), strides = (1,1), padding = 'valid', name = conv_name_base + '2a')(X)
+    X = BatchNormalization(axis = 3, name = bn_name_base + '2a')(X)
+    X = Activation('relu')(X)
+    
+   
+    X = Conv2D(filters = F2, kernel_size = (f, f), strides = (1,1), padding = 'same', name = conv_name_base + '2b')(X)
+    X = BatchNormalization(axis = 3, name = bn_name_base + '2b')(X)
+    X = Activation('relu')(X)
+
+    
+    X = Conv2D(filters = F3, kernel_size = (1, 1), strides = (1,1), padding = 'valid', name = conv_name_base + '2c')(X)
+    X = BatchNormalization(axis = 3, name = bn_name_base + '2c')(X)
+
+    # Final step: Add shortcut value to main path, and pass it through a RELU activation (≈2 lines)
+    X = layers.Add()([X, X_shortcut])
+    X = Activation('relu')(X)
+    
+    ### END CODE HERE ###
+    
+    return X
+
+def convolutional_block(X, f, filters, stage, block, s = 2):
+    """
+    Implementation of the convolutional block
+    
+    Arguments:
+    X -- input tensor of shape (m, n_H_prev, n_W_prev, n_C_prev)
+    f -- integer, specifying the shape of the middle CONV's window for the main path
+    filters -- python list of integers, defining the number of filters in the CONV layers of the main path
+    stage -- integer, used to name the layers, depending on their position in the network
+    block -- string/character, used to name the layers, depending on their position in the network
+    s -- Integer, specifying the stride to be used
+    
+    Returns:
+    X -- output of the convolutional block, tensor of shape (n_H, n_W, n_C)
+    """
+    
+    # defining name basis
+    conv_name_base = 'res' + str(stage) + block + '_branch'
+    bn_name_base = 'bn' + str(stage) + block + '_branch'
+    
+    # Retrieve Filters
+    F1, F2, F3 = filters
+    
+    # Save the input value
+    X_shortcut = X
+
+
+    
+    X = Conv2D(F1, (1, 1), strides = (s,s), name = conv_name_base + '2a', padding='valid')(X)
+    X = BatchNormalization(axis = 3, name = bn_name_base + '2a')(X)
+    X = Activation('relu')(X)
+    
+    
+    X = Conv2D(F2, (f, f), strides = (1,1), name = conv_name_base + '2b', padding='same')(X)
+    X = BatchNormalization(axis = 3, name = bn_name_base + '2b')(X)
+    X = Activation('relu')(X)
+
+    
+    X = Conv2D(F3, (1, 1), strides = (1,1), name = conv_name_base + '2c', padding='valid')(X)
+    X = BatchNormalization(axis = 3, name = bn_name_base + '2c')(X)
+
+   
+    X_shortcut = Conv2D(F3, (1, 1), strides = (s,s), name = conv_name_base + '1', padding='valid')(X_shortcut)
+    X_shortcut = BatchNormalization(axis = 3, name = bn_name_base + '1')(X_shortcut)
+
+    
+    X = layers.Add()([X, X_shortcut])
+    X = Activation('relu')(X)
+    
+   
+    
+    return X
+
+def ResNet50(input_shape = (32, 32, 3), classes = 10):
+    """
+    Implementation of the popular ResNet50 the following architecture:
+    CONV2D -> BATCHNORM -> RELU -> MAXPOOL -> CONVBLOCK -> IDBLOCK*2 -> CONVBLOCK -> IDBLOCK*3
+    -> CONVBLOCK -> IDBLOCK*5 -> CONVBLOCK -> IDBLOCK*2 -> AVGPOOL -> TOPLAYER
+
+    Arguments:
+    input_shape -- shape of the images of the dataset
+    classes -- integer, number of classes
+
+    Returns:
+    model -- a Model() instance in Keras
+    """
+    
+    X_input = Input(input_shape)
+
+    
+    # Zero-Padding
+    X = ZeroPadding2D((3, 3))(X_input)
+    
+    # Stage 1
+    X = Conv2D(64, (7, 7), strides = (2, 2), name = 'conv1')(X)
+    X = BatchNormalization(axis = 3, name = 'bn_conv1')(X)
+    X = Activation('relu')(X)
+    X = MaxPooling2D((3, 3), strides=(2, 2))(X)
+
+    # Stage 2
+    X = convolutional_block(X, f = 3, filters = [32, 32, 128], stage = 2, block='a', s = 1)
+    X = identity_block(X, 3, [32, 32, 128], stage=2, block='b')
+    X = identity_block(X, 3, [32, 32, 128], stage=2, block='c')
+
+
+    # Stage 3
+    X = convolutional_block(X, f=3, filters=[64,64,256], s=2, block='a', stage=3)
+    X = identity_block(X, 3, [64,64,256], stage=3, block='b')
+    X = identity_block(X, 3, [64,64,256], stage=3, block='c')
+    X = identity_block(X, 3, [64,64,256], stage=3, block='d')
+
+    # Stage 4
+    X = convolutional_block(X, f=3, filters=[128, 128, 512], s=2, block='a', stage=4)
+    X = identity_block(X, 3, [128, 128, 512], stage=4, block='b')
+    X = identity_block(X, 3, [128, 128, 512], stage=4, block='c')
+    X = identity_block(X, 3, [128, 128, 512], stage=4, block='d')
+    X = identity_block(X, 3, [128, 128, 512], stage=4, block='e')
+    X = identity_block(X, 3, [128, 128, 512], stage=4, block='f')
+    print(X.shape)
+    print(X.shape)
+    X = AveragePooling2D()(X)
+
+    # output layer
+    X = Flatten()(X)
+    print(X.shape)
+    X = Dense(100, activation='relu')(X)
+    X = Dense(classes, activation='softmax', name='fc' + str(classes))(X)
+    
+    
+    # Create model
+    model = Model(inputs = X_input, outputs = X, name='ResNet50')
+
+    return model
+
+model = ResNet50()
+model.compile(loss='categorical_crossentropy',
+              optimizer=Adam(lr=0.002),
+              metrics=['accuracy'])
+
+model.fit(X_train, y_train, batch_size=100, epochs=30, validation_split=0.1,  shuffle=True)
+
+predictions_test = np.argmax(model.predict(X_test), axis=1)
+
+prediction_test_df  = pd.DataFrame(index = np.arange(0, len(X_test)))
+prediction_test_df["Category"]= predictions_test
+prediction_test_df.to_csv("submission5.csv", index_label='Id')
+model.save_weights("resNetV5")
+
+print('Done')
+
+
+
+
